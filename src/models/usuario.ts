19aa7f1c9cf1ed9475eea16472def552e367018a
JSegovia <acosta.segoviaa@hotmail.com>
2021-12-19 20:04:24 -0500

    uid

--- src/models/usuario.ts
+++ src/models/usuario.ts
@@ -32,7 +32,10 @@ const UsuarioSchema = new Schema<Usuario>({
 });
 
 UsuarioSchema.methods.toJSON = function () {
-  const { __v, password, ...usuario } = this.toObject();
+  const { __v, password, _id, ...usuario } = this.toObject();
+
+  usuario.uid = _id;
+
   return usuario;
 };
 
