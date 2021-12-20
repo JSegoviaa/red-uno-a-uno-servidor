fb42f968cfc78bbbb71c02a3c0487c1b7c409ac3
JSegovia <acosta.segoviaa@hotmail.com>
2021-12-19 21:01:20 -0500

    valida roles

--- src/middlewares/validarRoles.ts
+++ src/middlewares/validarRoles.ts
@@ -1,10 +1,6 @@
 import { NextFunction, Response } from 'express';
 
-export const esAdminRol = async (
-  req: any,
-  res: Response,
-  next: NextFunction
-) => {
+export const esAdminRol = (req: any, res: Response, next: NextFunction) => {
   if (!req.usuario) {
     return res
       .status(500)
@@ -13,10 +9,28 @@ export const esAdminRol = async (
 
   const { role, nombre } = req.usuario;
   if (role !== 'Administrador') {
-    return res
-      .status(401)
-      .json({ msg: `${nombre} no tiene permisos para realizar esa acción` });
+    return res.status(401).json({
+      msg: `${nombre} no tiene permisos de Administrador para realizar esa acción`,
+    });
   }
 
   next();
+};
+
+export const tieneRol = (...roles: string[]) => {
+  return (req: any, res: Response, next: NextFunction) => {
+    if (!req.usuario) {
+      return res
+        .status(500)
+        .json({ msg: 'Se quiere verificar el rol sin validar el token' });
+    }
+
+    if (!roles.includes(req.usuario.role)) {
+      return res.status(401).json({
+        msg: `Para realizar esta acción se requiere alguno de estos roles: ${roles}`,
+      });
+    }
+
+    next();
+  };
 };
