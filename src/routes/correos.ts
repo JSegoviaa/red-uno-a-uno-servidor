e8afebee0de348fa0f831d075e5115d40dbfa5cd
JSegovia <acosta.segoviaa@hotmail.com>
2021-12-25 15:51:56 -0500

    validacion del contacto form

--- src/routes/correos.ts
+++ src/routes/correos.ts
@@ -11,7 +11,15 @@ router.post(
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('apellido', 'El apellido es obligatorio').not().isEmpty(),
     check('telefono', 'El teléfono es obligatorio').not().isEmpty(),
+    check(
+      'telefono',
+      'El número de teléfono debe de tener al menos 10 dígitos'
+    ).isLength({ min: 10 }),
     check('mensaje', 'El mensaje es obligatorio').not().isEmpty(),
+    check(
+      'mensaje',
+      'El mensaje debe de tener al menos 30 caracteres'
+    ).isLength({ min: 30 }),
     check('correo', 'El correo electrónico ingresado no es correcto').isEmail(),
     validarCampos,
   ],
