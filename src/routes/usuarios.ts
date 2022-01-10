4a9e697029b3e5076c62f705d20b216909612bae
JSegovia <acosta.segoviaa@gmamil.com>
2022-01-10 13:39:10 -0500

    validación del length del número telefónico

--- src/routes/usuarios.ts
+++ src/routes/usuarios.ts
@@ -1,64 +1,72 @@
-import { Router } from 'express';
-import { check } from 'express-validator';
+import { Router } from "express";
+import { check } from "express-validator";
 import {
   actualizarUsuario,
   crearUsuario,
   eliminarUsuario,
   obtenerUsuario,
   obtenerUsuarios,
-} from '../controllers/usuarios';
+} from "../controllers/usuarios";
 import {
   esRolValido,
   existeCorreo,
   existeUsuarioPorId,
-} from '../helpers/dbValidators';
-import { validarCampos } from '../middlewares/validarCampos';
-import { validarJWT } from '../middlewares/validarJWT';
-import { esAdminRol } from '../middlewares/validarRoles';
+} from "../helpers/dbValidators";
+import { validarCampos } from "../middlewares/validarCampos";
+import { validarJWT } from "../middlewares/validarJWT";
+import { esAdminRol } from "../middlewares/validarRoles";
 
 const router = Router();
 
-router.get('/', obtenerUsuarios);
-router.get('/:id', obtenerUsuario);
+router.get("/", obtenerUsuarios);
+router.get("/:id", obtenerUsuario);
 
 router.post(
-  '/',
+  "/",
   [
-    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
-    check('apellido', 'El apellido es obligatorio').not().isEmpty(),
-    check('correo', 'El correo electrónico ingresado no es correcto').isEmail(),
-    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("apellido", "El apellido es obligatorio").not().isEmpty(),
+    check("correo", "El correo electrónico ingresado no es correcto").isEmail(),
+    check("password", "La contraseña es obligatoria").not().isEmpty(),
     check(
-      'password',
-      'La contraseña debe de tener al menos 6 caracteres'
+      "password",
+      "La contraseña debe de tener al menos 6 caracteres"
     ).isLength({ min: 6 }),
-    check('correo').custom(existeCorreo),
-    check('role').custom(esRolValido),
+    check("correo").custom(existeCorreo),
+    check("role").custom(esRolValido),
     validarCampos,
   ],
   crearUsuario
 );
 
 router.put(
-  '/:id',
+  "/:id",
   [
-    check('id', 'No es un id válido').isMongoId(),
-    check('id').custom(existeUsuarioPorId),
+    check("id", "No es un id válido").isMongoId(),
+    check("id").custom(existeUsuarioPorId),
     // check('role').custom(esRolValido),
     // check('correo', 'El correo electrónico ingresado no es correcto').isEmail(),
-    check('correo').custom(existeCorreo),
+    check(
+      "telefonoOficina",
+      "El número telefónico debe tener 10 caracteres"
+    ).isLength({ min: 10, max: 10 }),
+    check(
+      "telefonoPersonal",
+      "El número telefónico debe tener 10 caracteres"
+    ).isLength({ min: 10, max: 10 }),
+    check("correo").custom(existeCorreo),
     validarCampos,
   ],
   actualizarUsuario
 );
 
 router.delete(
-  '/:id',
+  "/:id",
   [
     validarJWT,
     esAdminRol,
-    check('id', 'No es un id válido').isMongoId(),
-    check('id').custom(existeUsuarioPorId),
+    check("id", "No es un id válido").isMongoId(),
+    check("id").custom(existeUsuarioPorId),
     validarCampos,
   ],
   eliminarUsuario
