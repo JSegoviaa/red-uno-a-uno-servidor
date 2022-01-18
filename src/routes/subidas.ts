37d62342c512f049e9aa5b977ebceaebf85ec234
JSegovia <acosta.segoviaa@gmamil.com>
2022-01-18 17:00:16 -0500

    cloudinary

--- src/routes/subidas.ts
+++ src/routes/subidas.ts
@@ -14,9 +14,9 @@ import {
 const router = Router();
 
 v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "du6f7alxg",
+  api_key: "575558682358524",
+  api_secret: "gFqgCCTVoJ-RrNufHsd8JHmIa3Y",
 });
 
 const storagePerfil = new CloudinaryStorage({
