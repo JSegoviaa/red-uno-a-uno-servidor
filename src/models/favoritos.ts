1bea845c564c8b63e57257b51f3368b9eb49d303
JSegovia <acosta.segoviaa@gmamil.com>
2022-01-24 17:27:53 -0500

    valor por defecto en agregar favoritos

--- src/models/favoritos.ts
+++ src/models/favoritos.ts
@@ -1,14 +1,18 @@
 import { Schema, model, Types } from "mongoose";
 
+type Solicitud = "Pendiente" | "Aprobado" | "Rechazado";
+
 interface Favorito {
   usuario: Types.ObjectId;
   inmueble: Types.ObjectId;
+  solicitud: Solicitud;
 }
 
 const FavoritoSchema = new Schema<Favorito>(
   {
     usuario: { type: Schema.Types.ObjectId, ref: "Usuario", required: true },
     inmueble: { type: Schema.Types.ObjectId, ref: "Inmueble", required: true },
+    solicitud: { type: String, required: true, default: "Pendiente" },
   },
   { timestamps: true }
 );
