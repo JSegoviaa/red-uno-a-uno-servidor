b276f1f25488236b51f1d35fd5645c0985153fc6
JSegovia <acosta.segoviaa@gmamil.com>
2022-02-16 13:04:27 -0500

    imgs

--- src/controllers/subidas.ts
+++ src/controllers/subidas.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
-import { Usuario } from "../models/usuario";
-import { Inmueble } from "../models/inmuebles";
-import { v2 } from "cloudinary";
+import { Request, Response } from 'express';
+import { Usuario } from '../models/usuario';
+import { Inmueble } from '../models/inmuebles';
+import { v2 } from 'cloudinary';
 
 export const subirFotoPerfil = async (req: Request, res: Response) => {
   const { id } = req.params;
@@ -9,9 +9,7 @@ export const subirFotoPerfil = async (req: Request, res: Response) => {
   const usuario = await Usuario.findById(id);
 
   if (!usuario) {
-    return res
-      .status(400)
-      .json({ ok: false, msg: "No existe un usuario con ese id" + id });
+    return res.status(400).json({ ok: false, msg: 'No existe un usuario con ese id' + id });
   }
 
   const url = req.file?.path;
@@ -22,7 +20,7 @@ export const subirFotoPerfil = async (req: Request, res: Response) => {
 
   res.json({
     ok: true,
-    msg: "La foto de perfil se ha subido con éxito",
+    msg: 'La foto de perfil se ha subido con éxito',
     usuario,
   });
 };
@@ -33,21 +31,17 @@ export const imagenesInmueble = async (req: any, res: Response) => {
   const inmueble = await Inmueble.findById(pid);
 
   if (!inmueble) {
-    return res
-      .status(400)
-      .json({ ok: false, msg: "No existe un inmueble con ese id" + uid });
+    return res.status(400).json({ ok: false, msg: 'No existe un inmueble con ese id' + uid });
   }
 
   if (inmueble.imgs.length > 0) {
     inmueble.imgs.map((img) => {
-      const nombreArr = img.split("/");
+      const nombreArr = img.split('/');
       const nombre = nombreArr[nombreArr.length - 1];
-      const [public_id] = nombre.split(".");
+      const [public_id] = nombre.split('.');
       console.log(public_id);
 
-      v2.uploader.destroy(
-        `red1a1/usuarios/${uid}/inmuebles/${pid}/${public_id}`
-      );
+      v2.uploader.destroy(`red1a1/usuarios/${uid}/inmuebles/${pid}/${public_id}`);
     });
   }
 
@@ -61,12 +55,34 @@ export const imagenesInmueble = async (req: any, res: Response) => {
 
   await inmueble?.save();
 
-  res.json({ ok: true, msg: "Se han subido las imágenes con éxito" });
+  res.json({ ok: true, msg: 'Se han subido las imágenes con éxito' });
+};
+
+export const actualizarImgs = async (req: any, res: Response) => {
+  const { uid, pid } = req.params;
+
+  const inmueble = await Inmueble.findById(pid);
+
+  if (!inmueble) {
+    return res.status(400).json({ ok: false, msg: 'No existe un inmueble con ese id' + uid });
+  }
+
+  const files = req.files;
+
+  const imgs = files.map((file: Express.Multer.File) => {
+    return file.path;
+  });
+
+  inmueble!.imgs = imgs;
+
+  await inmueble?.save();
+
+  res.json({ ok: true, msg: 'Se han subido las imágenes con éxito', files: imgs });
 };
 
 export const subirVideo = async (req: Request, res: Response) => {
   const { uid, pid } = req.params;
-  console.log(req.file?.path, " ?SDAFa ");
+  console.log(req.file?.path, ' ?SDAFa ');
 
-  res.json({ ok: true, msg: "El vídeo se ha subido exitosamente", pid, uid });
+  res.json({ ok: true, msg: 'El vídeo se ha subido exitosamente', pid, uid });
 };
--- src/routes/subidas.ts
+++ src/routes/subidas.ts
@@ -1,26 +1,19 @@
-import { Request, Router } from "express";
-import { check } from "express-validator";
-import { v2 } from "cloudinary";
-import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import {
-  imagenesInmueble,
-  subirFotoPerfil,
-  subirVideo,
-} from "../controllers/subidas";
-import { validarCampos } from "../middlewares/validarCampos";
-import { validarJWT } from "../middlewares/validarJWT";
-import {
-  existeInmueblePorId,
-  existeUsuarioPorId,
-} from "../helpers/dbValidators";
+import { Request, Router } from 'express';
+import { check } from 'express-validator';
+import { v2 } from 'cloudinary';
+import multer from 'multer';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import { actualizarImgs, imagenesInmueble, subirFotoPerfil, subirVideo } from '../controllers/subidas';
+import { validarCampos } from '../middlewares/validarCampos';
+import { validarJWT } from '../middlewares/validarJWT';
+import { existeInmueblePorId, existeUsuarioPorId } from '../helpers/dbValidators';
 
 const router = Router();
 
 v2.config({
-  cloud_name: "du6f7alxg",
-  api_key: "575558682358524",
-  api_secret: "gFqgCCTVoJ-RrNufHsd8JHmIa3Y",
+  cloud_name: 'du6f7alxg',
+  api_key: '575558682358524',
+  api_secret: 'gFqgCCTVoJ-RrNufHsd8JHmIa3Y',
 });
 
 const storagePerfil = new CloudinaryStorage({
@@ -28,7 +21,7 @@ const storagePerfil = new CloudinaryStorage({
   params: async (req: Request, file) => {
     return {
       folder: `red1a1/usuarios/${req.params.id}`,
-      public_id: "foto-de-perfil",
+      public_id: 'foto-de-perfil',
     };
   },
 });
@@ -46,7 +39,7 @@ const storage = new CloudinaryStorage({
   },
 });
 
-const upload = multer({ storage }).array("pictures", 20);
+const upload = multer({ storage }).array('pictures', 20);
 
 const storageVideo = new CloudinaryStorage({
   cloudinary: v2,
@@ -55,38 +48,47 @@ const storageVideo = new CloudinaryStorage({
 
     return {
       folder: `red1a1/usuarios/${uid}/inmuebles/${pid}`,
-      public_id: "video",
+      public_id: 'video',
     };
   },
 });
 
-const video = multer({ storage: storageVideo }).single("video");
+const video = multer({ storage: storageVideo }).single('video');
 
 router.post(
-  "/usuarios/:id",
-  [
-    validarJWT,
-    check("id", "No es un id válido").isMongoId(),
-    uploadPerfil.single("picture"),
-    validarCampos,
-  ],
+  '/usuarios/:id',
+  [validarJWT, check('id', 'No es un id válido').isMongoId(), uploadPerfil.single('picture'), validarCampos],
   subirFotoPerfil
 );
 
 router.post(
-  "/:uid/:pid",
+  '/:uid/:pid',
   [
     validarJWT,
-    check("uid", "No es un id válido").isMongoId(),
-    check("uid").custom(existeUsuarioPorId),
-    check("pid", "No es un id válido").isMongoId(),
-    check("pid").custom(existeInmueblePorId),
+    check('uid', 'No es un id válido').isMongoId(),
+    check('uid').custom(existeUsuarioPorId),
+    check('pid', 'No es un id válido').isMongoId(),
+    check('pid').custom(existeInmueblePorId),
     upload,
     validarCampos,
   ],
   imagenesInmueble
 );
 
-router.post("/video/:uid/:pid", video, subirVideo);
+router.post(
+  '/actualizar/:uid/:pid',
+  [
+    validarJWT,
+    check('uid', 'No es un id válido').isMongoId(),
+    check('uid').custom(existeUsuarioPorId),
+    check('pid', 'No es un id válido').isMongoId(),
+    check('pid').custom(existeInmueblePorId),
+    upload,
+    validarCampos,
+  ],
+  actualizarImgs
+);
+
+router.post('/video/:uid/:pid', video, subirVideo);
 
 export default router;
