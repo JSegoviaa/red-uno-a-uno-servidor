e5d657ac49f4d7326a74412a65971ccbaabf919d
JSegovia <acosta.segoviaa@gmamil.com>
2022-02-17 14:34:19 -0500

    correo al usuario cuando compra un paquete y al adminsitrador

--- src/emails/pedido.ts
+++ src/emails/pedido.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from 'express';
 import nodemailer from 'nodemailer';
 
-export const contacto = async (req: Request, res: Response) => {
+export const nuevoPedido = async (req: Request, res: Response) => {
   const { nombre, apellido, correo, nombrePaquete, precio, importe, idCompra } = req.body;
   const contentHTML = `
     <h1>${nombre} ${apellido}</h1>
-    <h2>${correo}</h2>
+    <h2>${nombrePaquete}</h2>
+    <h2>${precio}</h2>
+    <h2>${importe}</h2>
+    <h2>${idCompra}</h2>
 `;
 
   const transport = nodemailer.createTransport({
@@ -23,6 +26,45 @@ export const contacto = async (req: Request, res: Response) => {
     from: 'Red1a1 <no-reply>@red1a1.com>',
     to: correo,
     subject: `¡Te has suscrito nuestro plan ${nombrePaquete}!`,
+    html: contentHTML,
+  };
+
+  const info = await transport.sendMail(mailOptions);
+
+  if (info.messageId) {
+    return res.json({ ok: true, msg: '' });
+  }
+
+  if (!info.messageId) {
+    return res.json({ ok: false, msg: '' });
+  }
+};
+
+export const nuevoPedidoAdmin = async (req: Request, res: Response) => {
+  const { nombre, apellido, nombrePaquete, precio, importe, idCompra } = req.body;
+  const contentHTML = `
+    <h1>${nombre} ${apellido}</h1>
+    <h2>${nombrePaquete}</h2>
+    <h2>${precio}</h2>
+    <h2>${importe}</h2>
+    <h2>${idCompra}</h2>
+`;
+
+  const transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+    tls: { rejectUnauthorized: false },
+  });
+
+  const mailOptions = {
+    from: 'Red1a1 <no-reply>@red1a1.com>',
+    to: process.env.SMTP_ENVIAR_A,
+    subject: `¡${nombre} ${apellido} se ha suscrito al plan ${nombrePaquete}!`,
     html: contentHTML,
   };
 
--- src/routes/correos.ts
+++ src/routes/correos.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
 import { contacto } from '../emails/contacto';
+import { nuevoPedido, nuevoPedidoAdmin } from '../emails/pedido';
 import { validarCampos } from '../middlewares/validarCampos';
 
 const router = Router();
@@ -11,19 +12,42 @@ router.post(
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('apellido', 'El apellido es obligatorio').not().isEmpty(),
     check('telefono', 'El teléfono es obligatorio').not().isEmpty(),
-    check(
-      'telefono',
-      'El número de teléfono debe de tener al menos 10 dígitos'
-    ).isLength({ min: 10 }),
+    check('telefono', 'El número de teléfono debe de tener al menos 10 dígitos').isLength({ min: 10 }),
     check('mensaje', 'El mensaje es obligatorio').not().isEmpty(),
-    check(
-      'mensaje',
-      'El mensaje debe de tener al menos 30 caracteres'
-    ).isLength({ min: 30 }),
+    check('mensaje', 'El mensaje debe de tener al menos 30 caracteres').isLength({ min: 30 }),
     check('correo', 'El correo electrónico ingresado no es correcto').isEmail(),
     validarCampos,
   ],
   contacto
+);
+
+router.post(
+  '/nuevo-pedido',
+  [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('apellido', 'El apellido es obligatorio').not().isEmpty(),
+    check('correo', 'El correo electrónico ingresado no es correcto').isEmail(),
+    check('nombrePaquete', 'El nombre del paquete es obligatorio').not().isEmpty(),
+    check('precio', 'El precio es obligatorio').not().isEmpty(),
+    check('importe', 'El precio es obligatorio').not().isEmpty(),
+    check('idCompra', 'El id es obligatorio').not().isEmpty(),
+    validarCampos,
+  ],
+  nuevoPedido
+);
+
+router.post(
+  '/nuevo-pedido-admin',
+  [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('apellido', 'El apellido es obligatorio').not().isEmpty(),
+    check('nombrePaquete', 'El nombre del paquete es obligatorio').not().isEmpty(),
+    check('precio', 'El precio es obligatorio').not().isEmpty(),
+    check('importe', 'El precio es obligatorio').not().isEmpty(),
+    check('idCompra', 'El id es obligatorio').not().isEmpty(),
+    validarCampos,
+  ],
+  nuevoPedidoAdmin
 );
 
 export default router;
